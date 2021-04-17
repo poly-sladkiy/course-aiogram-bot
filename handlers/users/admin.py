46f6f5258546b6a0f5568fe5b86cbb0e30bac31a
Konstantin Ignakov <ignakov37@gmail.com>
2021-04-17 12:42:52 +0300

    Изменена реакция на сообщение secret от админа

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -7,5 +7,5 @@ from loader import dp
 
 @dp.message_handler(IsPrivate(), text='secret', user_id=ADMINS)
 async def admin_chat_secret(message: types.Message):
-    await message.answer("Это секретное сообщение,"
-                         " вызванное одним из админов лс")
+    await message.answer(f"Это секретное сообщение,"
+                         f" вызванное <code>{message.chat.id}</code>")
